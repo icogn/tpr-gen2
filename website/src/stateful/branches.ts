23fb5d596b118ddf5e9f652ff32e8271791afd69
Isaac <iico7no2@gmail.com>
2023-06-22 21:32:54 -0500

    Fetch stable version tag working

--- website/src/stateful/branches.ts
+++ website/src/stateful/branches.ts
@@ -1,21 +1,29 @@
-type RawBranchInfo = {
+interface GithubReleaseInfo {
+  readonly tag_name: string;
+}
+
+type RawChannelInfo = {
   name: string;
   owner: string;
   repo: string;
   site: URL;
 };
 
-export interface BranchInfo extends RawBranchInfo {
+interface ChannelInfo extends RawChannelInfo {
   channel: string;
+  // `latestVersion` can be missing since we have to fetch it for each
+  // individual channel and we don't want to fail the entire thing if we fail to
+  // get the version for a single channel.
+  latestVersion?: string;
 }
 
-const REQUEST_CACHE_DURATION = 5 * 60 * 1000; // 5 minutes
+const REQUEST_CACHE_DURATION = 10 * 60 * 1000; // 10 minutes
 
-let cachedData: BranchInfo[] = [];
-let lastRequestTime = -1;
+let cachedData: ChannelInfo[] = [];
+let lastCacheTimestamp = -1;
 
 function withinCacheTime() {
-  return Date.now() - lastRequestTime < REQUEST_CACHE_DURATION;
+  return Date.now() - lastCacheTimestamp < REQUEST_CACHE_DURATION;
 }
 
 export async function fetchBranchesConfig() {
@@ -31,25 +39,86 @@ export async function fetchBranchesConfig() {
     const config = JSON.parse(responseText);
 
     if (config && typeof config === 'object') {
-      const branchInfoArr: BranchInfo[] = [];
+      const channelInfoArr: ChannelInfo[] = [];
 
-      Object.keys(config).forEach(channel => {
-        const channelConfig = config[channel] as RawBranchInfo;
-        branchInfoArr.push({
-          ...channelConfig,
-          channel: channel === 'stable' ? '' : channel,
-        });
-      });
+      const keys = Object.keys(config);
+      for (let i = 0; i < keys.length; i++) {
+        const rawChannel = keys[i];
 
-      cachedData = branchInfoArr;
-      lastRequestTime = Date.now();
+        const channel = rawChannel === 'stable' ? '' : rawChannel;
+        const channelInfo = {
+          ...config[rawChannel],
+          channel,
+        };
 
-      // TODO: need to fetch version data for each branch.
+        const latestVersion = await fetchBranchLatestVersion(channelInfo);
+        channelInfo.latestVersion = latestVersion;
+
+        channelInfoArr.push(channelInfo);
+      }
+
+      cachedData = channelInfoArr;
+      lastCacheTimestamp = Date.now();
     }
   } catch (e) {
-    console.log('Failed to fetch branches config.');
-    console.log(e);
+    console.error('Failed to fetch branches config.');
+    console.error(e);
   }
 
   return cachedData;
+}
+
+async function fetchBranchLatestVersion(channelInfo: ChannelInfo): string | undefined {
+  try {
+    if (!channelInfo.channel) {
+      // Fetch the latest if the channel is '' (the stable channel)
+      return await getLatestTagName(channelInfo);
+    } else {
+      // Get the latest semver tag for the given channel in the list of recent
+      // releases returned from Github.
+    }
+  } catch (e) {
+    console.error(`Failed to fetch latestVersion for channel "${channelInfo.channel}":`);
+    console.error(e);
+  }
+
+  return undefined;
+}
+
+export function newUrlFromBase(
+  pathname: string,
+  baseUrl: URL,
+  addRandomQueryToAvoidCaching = false,
+): URL {
+  const result = new URL(pathname, baseUrl);
+  // search is not propagated (search is an empty string if not specified)
+  const search = baseUrl.search;
+  if (search != null && search.length !== 0) {
+    result.search = search;
+  } else if (addRandomQueryToAvoidCaching) {
+    result.search = `noCache=${Date.now().toString(32)}`;
+  }
+  return result;
+}
+
+async function getLatestTagName(channelInfo: ChannelInfo): Promise<string | undefined> {
+  // Do not use API for GitHub to avoid limit
+  const url = newUrlFromBase(
+    `/${channelInfo.owner}/${channelInfo.repo}/releases/latest`,
+    new URL('https://github.com/'),
+  );
+  try {
+    const releaseInfo: GithubReleaseInfo = await fetch(url, {
+      headers: {Accept: 'application/json'},
+    }).then(res => res.json());
+
+    return releaseInfo.tag_name;
+  } catch (e) {
+    console.error(
+      `Unable to find latest version on GitHub (${url}), please ensure a production release exists: ${
+        e.stack || e.message
+      }`,
+    );
+  }
+  return undefined;
 }
