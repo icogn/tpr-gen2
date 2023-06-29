efb7d3438b77f24b384f932c9cb61d1edd5809bc
Isaac <iico7no2@gmail.com>
2023-06-28 19:19:21 -0500

    Disable asSyncComponent lint warnings

--- website/src/components/asSyncComponent.ts
+++ website/src/components/asSyncComponent.ts
@@ -5,9 +5,11 @@ type AsyncFunctionComponent<P = object> = (
   ...args: Parameters<React.FunctionComponent<P>>
 ) => Promise<ReturnType<React.FunctionComponent<P>>>;
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function asSyncComponent<T extends AsyncFunctionComponent<any>>(
   component: T,
 ): React.FunctionComponent<T extends AsyncFunctionComponent<infer P> ? P : never> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return component as any;
 }
 
