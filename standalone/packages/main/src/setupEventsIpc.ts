b240480434e8ea9c27eeb674b8e572d28ec4c050
Isaac <iico7no2@gmail.com>
2023-07-11 18:42:39 -0500

    Empty setupEventsIpc function

--- standalone/packages/main/src/setupEventsIpc.ts
+++ standalone/packages/main/src/setupEventsIpc.ts
@@ -1,26 +1,24 @@
-import {ipcMain} from 'electron';
-import {websiteReadyEmitter} from './website/forkWebsiteProcess';
-import {dbPreparedEmitter} from './prisma/prepareDb';
-// import {autoUpdater} from 'electron-updater';
-import {IpcChannel} from '../../shared/ipcChannels';
+// import {ipcMain} from 'electron';
+// import {websiteReadyEmitter} from './website/forkWebsiteProcess';
+// import {dbPreparedEmitter} from './prisma/prepareDb';
+// // import {autoUpdater} from 'electron-updater';
+// import {IpcChannel} from '../../shared/ipcChannels';
 
 function setupEventsIpc() {
-  ipcMain.on(IpcChannel.askDatabaseReady, event => {
-    dbPreparedEmitter.onceOrPrev((success: boolean | undefined) => {
-      if (success != null && !event.sender.isDestroyed()) {
-        event.sender.send(IpcChannel.databaseReady, success);
-      }
-    });
-  });
-
-  ipcMain.on(IpcChannel.askWebsiteReady, event => {
-    websiteReadyEmitter.onceOrPrev((success: boolean | undefined) => {
-      if (success != null && !event.sender.isDestroyed()) {
-        event.sender.send(IpcChannel.websiteReady, success);
-      }
-    });
-  });
-
+  // ipcMain.on(IpcChannel.askDatabaseReady, event => {
+  //   dbPreparedEmitter.onceOrPrev((success: boolean | undefined) => {
+  //     if (success != null && !event.sender.isDestroyed()) {
+  //       event.sender.send(IpcChannel.databaseReady, success);
+  //     }
+  //   });
+  // });
+  // ipcMain.on(IpcChannel.askWebsiteReady, event => {
+  //   websiteReadyEmitter.onceOrPrev((success: boolean | undefined) => {
+  //     if (success != null && !event.sender.isDestroyed()) {
+  //       event.sender.send(IpcChannel.websiteReady, success);
+  //     }
+  //   });
+  // });
   // ipcMain.on(IpcChannel.cancelAutoinstall, () => {
   //   console.log('Setting autoUpdater.autoInstallOnAppQuit to false');
   //   autoUpdater.autoInstallOnAppQuit = false;
