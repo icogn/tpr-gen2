bfb1d413d9c98c2907d8bb7c6700c23474139ca3
Isaac <iico7no2@gmail.com>
2023-07-11 18:56:22 -0500

    Remove dbPreparedEmitterImport

--- standalone/packages/main/src/setupEventsIpc.ts
+++ standalone/packages/main/src/setupEventsIpc.ts
@@ -1,18 +1,19 @@
 import {ipcMain} from 'electron';
 // import {websiteReadyEmitter} from './website/forkWebsiteProcess';
-import {dbPreparedEmitter} from './prisma/prepareDb';
+// import {dbPreparedEmitter} from './prisma/prepareDb';
 // // import {autoUpdater} from 'electron-updater';
 // import {IpcChannel} from '../../shared/ipcChannels';
 
 function setupEventsIpc() {
   // ipcMain.on(IpcChannel.askDatabaseReady, event => {
   ipcMain.on('tpr:ask-database-ready', () => {
-    dbPreparedEmitter.onceOrPrev((success: boolean | undefined) => {
-      console.log(success);
-      // if (success != null && !event.sender.isDestroyed()) {
-      //   event.sender.send(IpcChannel.databaseReady, success);
-      // }
-    });
+    console.log('abc');
+    // dbPreparedEmitter.onceOrPrev((success: boolean | undefined) => {
+    //   console.log(success);
+    //   // if (success != null && !event.sender.isDestroyed()) {
+    //   //   event.sender.send(IpcChannel.databaseReady, success);
+    //   // }
+    // });
   });
 
   // ipcMain.on(IpcChannel.askWebsiteReady, event => {
