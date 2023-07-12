efe79f8d314fa612c0bdaa7d8bb059243c62c9c1
Isaac <iico7no2@gmail.com>
2023-07-11 20:20:50 -0500

    Get channelKey normally

--- standalone/packages/main/src/setupEventsIpc.ts
+++ standalone/packages/main/src/setupEventsIpc.ts
@@ -1,27 +1,8 @@
-import {app, ipcMain} from 'electron';
-import semver from 'semver';
-// import {ipcMain} from 'electron';
+import {ipcMain} from 'electron';
 import {websiteReadyEmitter} from './website/forkWebsiteProcess';
 // import {dbPreparedEmitter} from './prisma/prepareDb';
 // // import {autoUpdater} from 'electron-updater';
 import {IpcChannel} from '../../shared/ipcChannels';
-
-function computeChannelKey() {
-  // return 'exampleChanKey';
-  const appVersion = app.getVersion();
-  if (semver.parse(appVersion) == null) {
-    throw new Error(`Critical error: semver failed to parse app version "${appVersion}".`);
-  }
-  const prereleaseVal = semver.prerelease(appVersion);
-  if (prereleaseVal == null) {
-    return 'stable';
-  }
-  const channel = String(prereleaseVal[0]);
-  if (!channel) {
-    throw new Error(`Failed to parse channel from appVersion "${appVersion}".`);
-  }
-  return channel;
-}
 
 function setupEventsIpc() {
   ipcMain.on(IpcChannel.askDatabaseReady, () => {
