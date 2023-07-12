22fcf4afb6a124e879c7bb2bb2a671df921e3181
Isaac <iico7no2@gmail.com>
2023-07-11 20:29:21 -0500

    Restore setupEventsIpc

--- standalone/packages/main/src/setupEventsIpc.ts
+++ standalone/packages/main/src/setupEventsIpc.ts
@@ -1,7 +1,7 @@
 import {ipcMain} from 'electron';
 import {websiteReadyEmitter} from './website/forkWebsiteProcess';
 import {dbPreparedEmitter} from './prisma/prepareDb';
-// // import {autoUpdater} from 'electron-updater';
+import {autoUpdater} from 'electron-updater';
 import {IpcChannel} from '../../shared/ipcChannels';
 
 function setupEventsIpc() {
@@ -21,10 +21,10 @@ function setupEventsIpc() {
     });
   });
 
-  // ipcMain.on(IpcChannel.cancelAutoinstall, () => {
-  //   console.log('Setting autoUpdater.autoInstallOnAppQuit to false');
-  //   autoUpdater.autoInstallOnAppQuit = false;
-  // });
+  ipcMain.on(IpcChannel.cancelAutoinstall, () => {
+    console.log('Setting autoUpdater.autoInstallOnAppQuit to false');
+    autoUpdater.autoInstallOnAppQuit = false;
+  });
 }
 
 export default setupEventsIpc;
