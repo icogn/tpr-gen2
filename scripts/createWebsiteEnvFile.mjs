10bd61ddd782d7bbb158bc65d212a66ac9a99fe2
Isaac <iico7no2@gmail.com>
2023-07-20 18:15:50 -0500

    Add comment

--- scripts/createWebsiteEnvFile.mjs
+++ scripts/createWebsiteEnvFile.mjs
@@ -4,6 +4,9 @@ import getRootDir from './util/getRootDir.mjs';
 import {createEnvFileContents, prepareWebsiteEnv} from './prepareEnv.mjs';
 import {getVersion} from './util/getVersion.mjs';
 
+// Note: it is okay to not pass in the version since the image should only ever
+// be built when the current version is correct. If we fetch an image to deploy,
+// the env is already baked into that image.
 function createWebsiteEnvFile() {
   const rootDir = getRootDir();
   const tmpDir = path.join(rootDir, 'tmp');
