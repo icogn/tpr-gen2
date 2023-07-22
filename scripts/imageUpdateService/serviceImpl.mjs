2fc479a7255fff90a1ad4445ef5584be99d1fda6
Isaac <iico7no2@gmail.com>
2023-07-21 20:27:49 -0500

    Simplify serviceImpl.mjs

--- scripts/imageUpdateService/serviceImpl.mjs
+++ scripts/imageUpdateService/serviceImpl.mjs
@@ -1,14 +1,9 @@
 // import {EventLogger} from 'node-windows';
-import fs from 'fs-extra';
-import path from 'node:path';
 import {spawnSync} from 'node:child_process';
 import semver from 'semver';
-// import getChannelInfo from './getChannelInfo.mjs';
 import getChannelLatestReleaseInfo from './getChannelLatestReleaseInfo.mjs';
-import getRootDir from '../util/getRootDir.mjs';
-import getImageStackHash from '../githubRelease/getImageStackHash.mjs';
-import downloadFile from './downloadFile.mjs';
 import getYarnCommand from '../util/getYarnCommand.mjs';
+import findContainerForChannelKey from '../util/docker/findSingleContainerForChannelKey.mjs';
 
 // const log = new EventLogger({
 //   source: 'My Event Log',
@@ -23,83 +18,17 @@ import getYarnCommand from '../util/getYarnCommand.mjs';
 
 // const args = 'dev';
 
-function parseContainerLine(line, desiredChannelKey) {
-  const reg = /([0-9a-f]+)\s+(tpr-generator:([a-z0-9:.\-_]+))/i;
+//
 
-  const match = line.match(reg);
-  if (!match) {
-    return null;
-  }
+// Check for if there is a newer version.
+// If there is, call deploy with fetch and that tag.
 
-  const imageVersion = match[3];
-
-  if (semver.parse(imageVersion) == null) {
-    return null;
-  }
-
-  const prerelease = semver.prerelease(imageVersion);
-
-  if (
-    (prerelease == null && desiredChannelKey === 'stable') ||
-    desiredChannelKey === prerelease[0]
-  ) {
-    return {
-      containerId: match[1],
-      image: match[2],
-      imageVersion: match[3],
-    };
-  }
-
-  return null;
-}
-
-function findContainerForChannelKey(channelKey) {
-  const result = spawnSync('docker', ['ps']);
-  const output = result.stdout.toString();
-
-  if (!output.startsWith('CONTAINER ID')) {
-    return null;
-  }
-
-  const lines = output.split(/[\r\n]+/);
-
-  let ret = null;
-
-  for (let i = 1; i < lines.length; i++) {
-    const parsed = parseContainerLine(lines[i], channelKey);
-    if (parsed) {
-      if (ret) {
-        // Act as if there is nothing to do if we find multiple running containers
-        // for this channels since we don't know which one to replace.
-        return null;
-      } else {
-        ret = parsed;
-      }
-    }
-  }
-
-  return ret;
-}
-
-function doWork(channelKey) {
-  const ret = findContainerForChannelKey(channelKey);
-  if (!ret) {
-    return;
-  }
-
-  // If there is a running container which we might replace, then we check to
-  // see if there is an update.
-
-  // Get latest version from github
-
-  console.log(ret);
-}
-
-async function main() {
-  const channelKey = 'dev';
-
-  const container = findContainerForChannelKey(channelKey);
-  if (!container) {
+async function processChannelKey(channelKey) {
+  // Service only replaces containers; it does not deploy one if there is not
+  // already one deployed or if there are multiple so we do not know which one
+  // to replace.
+  const {containerInfo} = findContainerForChannelKey(channelKey);
+  if (!containerInfo) {
     return;
   }
 
@@ -109,98 +38,37 @@ async function main() {
     channelKey,
   });
   console.log(latestReleaseInfo);
-
   if (!latestReleaseInfo) {
     return;
   }
 
-  if (semver.gt(latestReleaseInfo.version, container.imageVersion)) {
+  if (semver.gt(latestReleaseInfo.version, containerInfo.imageVersion)) {
     console.log('should replace');
-    await tryReplaceContainer({container, latestReleaseInfo});
+    // await tryReplaceContainer({containerInfo, latestReleaseInfo});
+
+    // Run deploy
+    spawnSync(getYarnCommand(), [
+      'deploy',
+      '--from-service',
+      '-f',
+      '-i',
+      latestReleaseInfo.version,
+    ]);
   } else {
     console.log('does not need to replace');
-    return;
-  }
-
-  // Check if latestRelease is a newer version than our current
-
-  // const a = await getChannelInfo();
-  // console.log(a);
-
-  const channelsToCheck = ['dev'];
-
-  for (let i = 0; i < channelsToCheck.length; i++) {
-    const channelKey = channelsToCheck[i];
-    doWork(channelKey);
   }
 }
 
-async function tryReplaceContainer({container, latestReleaseInfo}) {
-  const rootDir = getRootDir();
-  const tmpDir = path.join(rootDir, 'tmp');
+async function main() {
+  const channelKey = 'dev';
 
-  // Clean up all old files.
-  fs.ensureDirSync(tmpDir);
-  fs.emptyDirSync(tmpDir);
+  // TODO: fetch centralized channel config once up-front.
 
-  // Check that image from github is supported in terms of deploy hash. Bail if
-  // not compatible. Some way to alert this from service?
-  const asset = getAssetIfValid(latestReleaseInfo.release);
-  console.log(asset);
-
-  // If compatible, download the image from github. If fail, then just fails.
-  const downloadResult = await downloadFile(asset.browser_download_url, tmpDir);
-  if (downloadResult.downloadStatus != 'COMPLETE') {
-    throw new Error(
-      `Failed to download file. downloadStatus is "${downloadResult.downloadStatus}".`,
-    );
+  try {
+    await processChannelKey(channelKey);
+  } catch (e) {
+    console.error(e);
   }
-
-  // Temp, so we don't spam github with a ton of downloads in a row while
-  // testing.
-  const imagePath = path.join(tmpDir, 'tpr-generator_1.1.5-dev.35_60ae946a.tar');
-
-  // Load downloaded image. If fails, it fails.
-  const loadedImage = loadDockerImage(imagePath);
-  if (!loadedImage) {
-    throw new Error(`Failed to load docker image at path "${imagePath}".`);
-  }
-
-  // Stop current container
-  stopDockerContainer(container.containerId);
-
-  // Deploy the new container passing the imageVersion as an arg.
-  runDeploy(latestReleaseInfo.version);
-}
-
-function getAssetIfValid(release) {
-  const imageStackHash = getImageStackHash();
-  for (const asset of release.assets) {
-    if (asset.name.startsWith('tpr-generator_') && asset.name.endsWith(`_${imageStackHash}.tar`)) {
-      return asset;
-    }
-  }
-  return null;
-}
-
-function loadDockerImage(imagePath) {
-  const result = spawnSync('docker', ['load', '-i', imagePath]);
-  return result.status === 0;
-}
-
-function stopDockerContainer(containerId) {
-  const result = spawnSync('docker', ['stop', containerId]);
-  return result.status === 0;
-}
-
-function runDeploy(imageTag) {
-  console.log('runDeploy');
-  console.log(process.cwd());
-
-  const result = spawnSync(getYarnCommand(), ['deploy', '-i', imageTag], {
-    stdio: 'inherit',
-  });
-  return result.status === 0;
 }
 
 main();
