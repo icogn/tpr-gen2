d162c4150380c6e173043188c9466ceb0d926856
Isaac <iico7no2@gmail.com>
2023-07-24 21:52:15 -0500

    Update nextauth_url to trackerp site

--- serverStarter/startServer.mjs
+++ serverStarter/startServer.mjs
@@ -17,7 +17,8 @@ const result = spawnSync('node', ['/app/website-standalone/website/server.js'], 
     // TODO: this should come from an actual secret
     NEXTAUTH_SECRET: 'publiclyVisibleNextAuthSecret',
     // This appears to be used to build the call on the client-side.
-    NEXTAUTH_URL: 'http://localhost:2999',
+    // NEXTAUTH_URL: 'http://localhost:2999',
+    NEXTAUTH_URL: 'http://new.trackerp.net',
   },
 });
 
