885eead45b645519ff9078d222541dfc8ac26926
Isaac <iico7no2@gmail.com>
2023-07-28 22:19:15 -0500

    Version to 1.1.5-dev.47; check for https client-side

--- website/src/app/auth/login/LoginPageClient.tsx
+++ website/src/app/auth/login/LoginPageClient.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-import {useRef, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import styles from './LoginPageClient.module.css';
 import {signIn} from 'next-auth/react';
 import {useRouter} from 'next/navigation';
 
-type LoginPageClientProps = {
-  canShowLoginBtn: boolean;
-};
+enum BtnState {
+  Loading,
+  Show,
+  Hide,
+}
 
 function calculateCallbackUrl() {
   try {
@@ -18,15 +20,34 @@ function calculateCallbackUrl() {
   }
 }
 
-export default function LoginPageClient({canShowLoginBtn}: LoginPageClientProps) {
+export default function LoginPageClient() {
   const router = useRouter();
 
   const username = useRef('');
   const password = useRef('');
-  const [error, setError] = useState<string>('');
+  const [error, setError] = useState('');
+  const [btnState, setBtnState] = useState<BtnState>(BtnState.Loading);
+
+  useEffect(() => {
+    try {
+      const url = new URL(window.location.href);
+      console.log(url);
+      if (
+        url.protocol === 'https:' ||
+        url.hostname === 'localhost' ||
+        url.hostname === '127.0.0.1'
+      ) {
+        setBtnState(BtnState.Show);
+      } else {
+        setBtnState(BtnState.Hide);
+      }
+    } catch (e) {
+      setBtnState(BtnState.Hide);
+    }
+  }, []);
 
   const onSubmit = async () => {
-    if (!canShowLoginBtn) {
+    if (btnState !== BtnState.Show) {
       return;
     }
 
@@ -57,7 +78,6 @@ export default function LoginPageClient({canShowLoginBtn}: LoginPageClientProps)
     <div className={styles.root}>
       <div className={styles.loginSection}>
         <div>LoginPagee</div>
-        <div>{`canShowLoginBtn:${canShowLoginBtn}`}</div>
         <div style={{color: 'black'}}>
           <div>
             <input
@@ -77,11 +97,8 @@ export default function LoginPageClient({canShowLoginBtn}: LoginPageClientProps)
             />
           </div>
         </div>
-        {canShowLoginBtn ? (
-          <button onClick={onSubmit}>the button</button>
-        ) : (
-          <div>Site must be https in order to log in.</div>
-        )}
+        {btnState === BtnState.Show && <button onClick={onSubmit}>the button</button>}
+        {btnState === BtnState.Hide && <div>Site must be https in order to log in.</div>}
         {error && <div className={styles.errorBox}>{error}</div>}
       </div>
     </div>
--- website/src/app/auth/login/page.tsx
+++ website/src/app/auth/login/page.tsx
@@ -1,27 +1,7 @@
-import {headers} from 'next/headers';
 import {getServerSession} from 'next-auth';
 import {authOptions} from '@/app/api/auth/[...nextauth]/route';
 import LoginPageClient from './LoginPageClient';
 import {redirect} from 'next/navigation';
-
-function calcCanShowLoginBtn() {
-  try {
-    const hdrs = headers();
-    console.log('headers:');
-    console.log([...hdrs.entries()]);
-    const host = hdrs.get('x-forwarded-host');
-    console.log(`host is ${host}`);
-    const hostname = host ? host.split(':')[0] : '';
-    console.log(`hostname is "${hostname}"`);
-
-    const xf = hdrs.get('x-forwarded-proto');
-    console.log(`x-forwarded-proto is "${xf}"`);
-
-    return xf === 'https' || hostname === 'localhost' || hostname === '127.0.0.1';
-  } catch (e) {
-    return false;
-  }
-}
 
 export default async function LoginPage() {
   const session = await getServerSession(authOptions);
@@ -29,7 +9,5 @@ export default async function LoginPage() {
     redirect('/');
   }
 
-  const canShowLoginBtn = calcCanShowLoginBtn();
-
-  return <LoginPageClient canShowLoginBtn={canShowLoginBtn} />;
+  return <LoginPageClient />;
 }
