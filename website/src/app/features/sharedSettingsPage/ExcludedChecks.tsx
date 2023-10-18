bbf68f3a4981d97cf7d94c58e6acb86cbd985247
Isaac <iico7no2@gmail.com>
2023-10-17 20:09:52 -0500

    Add react-virtuoso

--- website/src/app/features/sharedSettingsPage/ExcludedChecks.tsx
+++ website/src/app/features/sharedSettingsPage/ExcludedChecks.tsx
@@ -1,27 +1,53 @@
 'use client';
 
-import { FixedSizeList as List } from 'react-window';
+// import { FixedSizeList as List } from 'react-window';
 
-type RowProps = {
-  index: number;
-  style: React.CSSProperties;
-};
+// type RowProps = {
+//   index: number;
+//   style: React.CSSProperties;
+// };
 
-function Row({ index, style }: RowProps) {
-  return <div style={style}>Row {index}</div>;
-}
+// function Row({ index, style }: RowProps) {
+//   return <div style={style}>Row {index}</div>;
+// }
+
+// function ExcludedChecks() {
+//   return (
+//     <div>
+//       <List
+//         height={500}
+//         itemCount={1000}
+//         itemSize={35}
+//         width={300}
+//       >
+//         {Row}
+//       </List>
+//     </div>
+//   );
+// }
+
+// export default ExcludedChecks;
+
+import { Virtuoso } from 'react-virtuoso';
+import { excludedChecksList } from './excludedChecksList';
+
+// type RowProps = {
+//   index: number;
+//   style: React.CSSProperties;
+// };
+
+// function Row({ index, style }: RowProps) {
+//   return <div style={style}>Row {index}</div>;
+// }
 
 function ExcludedChecks() {
   return (
     <div>
-      <List
-        height={500}
-        itemCount={1000}
-        itemSize={35}
-        width={300}
-      >
-        {Row}
-      </List>
+      <Virtuoso
+        style={{ height: '400px' }}
+        totalCount={excludedChecksList.length}
+        itemContent={index => <div>{`${index} ${excludedChecksList[index]}`}</div>}
+      />
     </div>
   );
 }
